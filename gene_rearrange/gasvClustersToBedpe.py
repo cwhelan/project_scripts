b771b7e9a4c14df5e79918e767d65bebb9993891
Chris Whelan <cwhelan@gmail.com>
2012-06-25 12:26:16 -0700

    fixes to gasv script

--- gene_rearrange/gasvClustersToBedpe.py
+++ gene_rearrange/gasvClustersToBedpe.py
@@ -5,8 +5,10 @@ import sys
 clusterFile = open(sys.argv[1], 'r')
 
 for line in clusterFile:
-    if line.startswith('#'):
-        next
+    #print line
+    if line.startswith("#"):
+        #  print "starts with #"
+        continue
     fields = line.split()
     cid = fields[0]
     c1 = fields[1]
@@ -15,19 +17,20 @@ for line in clusterFile:
     e1 = region1[0]
     c2 = fields[3]
     region2 = fields[4].split(",")
+    #    print region2
     s2 = region2[0]
     e2 = region2[1]
     numPairs = fields[5]
     localization = fields[6]
     breaktype = fields[7]
     bedpeid = ";".join([cid, numPairs, localization, breaktype])
-    s1 = "+"
-    s2 = "-"
+    o1 = "+"
+    o2 = "-"
     if breaktype == "IR" or breaktype == "I+":
-        s1 = "+"
-        s2 = "+"
+        o1 = "+"
+        o2 = "+"
     elif breaktype == "I-":
-        s1 = "-"
-        s2 = "-"
-    print "\t".join(c1, s1, e1, c2, s2, e2, bedpeid, numPairs, s1, s2)
+        o1 = "-"
+        o2 = "-"
+    print "\t".join([c1, s1, e1, c2, s2, e2, bedpeid, numPairs, o1, o2])
     
