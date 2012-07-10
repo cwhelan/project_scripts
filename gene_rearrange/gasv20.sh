31e1139acb313783e13025a63ad2a6acebf02e4a
Chris Whelan <cwhelan@gmail.com>
2012-07-10 10:57:06 -0700

    More memory for GASV

--- gene_rearrange/gasv20.sh
+++ gene_rearrange/gasv20.sh
@@ -10,11 +10,11 @@ GASVPRO_HOME=/g/whelanch/software/GASVProSrc
 
 FILE_PREFIX=`echo $BAM_FILE | awk -F. '{ print $1 }'`
 
-echo "java -Xms512m -Xmx2048m -jar ${GASV_HOME}/bin/BAMToGASV.jar $BAM_FILE"
-java -Xms512m -Xmx2048m -jar ${GASV_HOME}/bin/BAMToGASV.jar $BAM_FILE
+echo "java -Xms512m -Xmx4096m -jar ${GASV_HOME}/bin/BAMToGASV.jar $BAM_FILE"
+java -Xms512m -Xmx4096m -jar ${GASV_HOME}/bin/BAMToGASV.jar $BAM_FILE
 
-echo "java -Xms512m -Xmx2048m -jar ${GASV_HOME}/bin/GASV.jar --minClusterSize 3 --output reads ${BAM_FILE}.gasv.in"
-java -Xms512m -Xmx2048m -jar ${GASV_HOME}/bin/GASV.jar --minClusterSize 3 --output reads --batch ${BAM_FILE}.gasv.in
+echo "java -Xms512m -Xmx4096m -jar ${GASV_HOME}/bin/GASV.jar --minClusterSize 3 --output reads ${BAM_FILE}.gasv.in"
+java -Xms512m -Xmx4096m -jar ${GASV_HOME}/bin/GASV.jar --minClusterSize 3 --output reads --batch ${BAM_FILE}.gasv.in
 
 #echo "Pruning Redundant/Overlapping Predictions...."
 ${GASVPRO_HOME}/scripts/pruneClusters.pl ${FILE_PREFIX}_${LIBRARY_NAME}.deletion.clusters ESP > prune_clusters.out
