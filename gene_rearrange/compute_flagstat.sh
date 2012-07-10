f42a41c8764ce5129196efe1beed1af8503d4be1
Chris Whelan <cwhelan@gmail.com>
2012-07-10 13:26:13 -0700

    Add flagstats to standard workflow

--- gene_rearrange/compute_flagstat.sh
+++ gene_rearrange/compute_flagstat.sh
@@ -6,4 +6,5 @@ set -u
 BAM_FILE=$1
 FILE_PREFIX=`echo $BAM_FILE | awk -F. '{ print $1 }'`
 
+echo "samtools flagstat $BAM_FILE > ${FILE_PREFIX}.flagstat.txt"
 samtools flagstat $BAM_FILE > ${FILE_PREFIX}.flagstat.txt
