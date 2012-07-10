600d7b0909a3d078e8b43d65a38efa2d841d446c
Chris Whelan <cwhelan@gmail.com>
2012-07-10 13:01:10 -0700

    script to merge multiple read group alignments

--- gene_rearrange/merge_read_groups.sh
+++ gene_rearrange/merge_read_groups.sh
@@ -8,3 +8,7 @@ PARENT_DIR=$1
 find $PARENT_DIR -name novoalign_tier1_sort_clean_mdup.bam | \
     sed 's/^/INPUT=/' |
     xargs java -jar /g/whelanch/software/picard-tools-1.72/MergeSamFiles.jar OUTPUT=$PARENT_DIR/novoalign_tier1_sort_clean_mdup.bam USE_THREADING=true
+
+find $PARENT_DIR -name novoalign_tier1_sort_clean_rmdup.bam | \
+    sed 's/^/INPUT=/' | 
+    xargs java -jar /g/whelanch/software/picard-tools-1.72/MergeSamFiles.jar OUTPUT=$PARENT_DIR/novoalign_tier1_sort_clean_rmdup.bam USE_THREADING=true
