b00ca9b0a7076e7715d6c824d21baff98d8a885c
Chris Whelan <cwhelan@gmail.com>
2012-08-11 20:05:29 -0700

    fix heap size

--- gene_rearrange/merge_read_groups.sh
+++ gene_rearrange/merge_read_groups.sh
@@ -7,15 +7,15 @@ PARENT_DIR=$1
 
 find $PARENT_DIR -name novoalign_tier1_sort_clean_mdup.bam | \
     sed 's/^/INPUT=/' |
-    xargs echo "java -Xmx4096m -jar /g/whelanch/software/picard-tools-1.72/MergeSamFiles.jar -Xmx=4096m OUTPUT=$PARENT_DIR/novoalign_tier1_sort_clean_mdup.bam USE_THREADING=true"
+    xargs echo "java -Xmx4096m -jar /g/whelanch/software/picard-tools-1.72/MergeSamFiles.jar  OUTPUT=$PARENT_DIR/novoalign_tier1_sort_clean_mdup.bam USE_THREADING=true"
 
 find $PARENT_DIR -name novoalign_tier1_sort_clean_mdup.bam | \
     sed 's/^/INPUT=/' |
-    xargs java -Xmx4096m -jar /g/whelanch/software/picard-tools-1.72/MergeSamFiles.jar -Xmx=4096m OUTPUT=$PARENT_DIR/novoalign_tier1_sort_clean_mdup.bam USE_THREADING=true
+    xargs java -Xmx4096m -jar /g/whelanch/software/picard-tools-1.72/MergeSamFiles.jar  OUTPUT=$PARENT_DIR/novoalign_tier1_sort_clean_mdup.bam USE_THREADING=true
 
 find $PARENT_DIR -name novoalign_tier1_sort_clean_rmdup.bam | \
     sed 's/^/INPUT=/' | 
-    xargs echo "java -Xmx4096m -jar /g/whelanch/software/picard-tools-1.72/MergeSamFiles.jar -Xmx=4096m OUTPUT=$PARENT_DIR/novoalign_tier1_sort_clean_rmdup.bam USE_THREADING=true"
+    xargs echo "java -Xmx4096m -jar /g/whelanch/software/picard-tools-1.72/MergeSamFiles.jar  OUTPUT=$PARENT_DIR/novoalign_tier1_sort_clean_rmdup.bam USE_THREADING=true"
 
 find $PARENT_DIR -name novoalign_tier1_sort_clean_rmdup.bam | \
     sed 's/^/INPUT=/' | 
