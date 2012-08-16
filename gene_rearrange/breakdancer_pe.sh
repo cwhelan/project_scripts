fbbb8b34b4350b88093b7014c0c244131e8b7aa6
Chris Whelan <cwhelan@gmail.com>
2012-08-16 14:48:55 -0700

    Add cutoff param to breakdancer

--- gene_rearrange/breakdancer_pe.sh
+++ gene_rearrange/breakdancer_pe.sh
@@ -25,10 +25,10 @@ FILE_PREFIX=`echo $FILE_NAME | awk -F. '{ print $1 }'`
 CONFIG_FILE=${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}_${CUTOFF}.cfg
 
 bam2cfg.pl -c $CUTOFF -q $QUALITY $WORKING_DIR/$BAM_FILE > $CONFIG_FILE
-BD_OUT_FILE=${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}.bd_out.txt
+BD_OUT_FILE=${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}_${CUTOFF}.bd_out.txt
 breakdancer_max -d ${FILE_PREFIX}.bdreads.bed -g ${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}.bd.bed $CONFIG_FILE -q $QUALITY -r $MIN_PAIRS -c $CUTOFF > $BD_OUT_FILE
 
 #cat $BD_OUT_FILE | awk '!/^#/ {OFS="\t"; print $1,$2,$5,$7":"$8,$9,"+"}' > ${FILE_PREFIX}.bd_regions.bed
-cat $BD_OUT_FILE | awk '!/^#/ && $1 == $4 {OFS="\t"; print $1,$2,$5,$7":"$8,$9,"+"}' > ${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}.bd_regions.bed
-cat $BD_OUT_FILE | awk '!/^#/ && $1 != $4 {OFS="\t"; print $1,$2,$2+1000,$7"("$1"-"$4"):"$8,$9,"+"}' >> ${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}.bd_regions.bed
-cat $BD_OUT_FILE | awk '!/^#/ && $1 != $4 {OFS="\t"; print $4,$5,$5+1000,$7"("$1"-"$4"):"$8,$9,"+"}' >> ${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}.bd_regions.bed
+cat $BD_OUT_FILE | awk '!/^#/ && $1 == $4 {OFS="\t"; print $1,$2,$5,$7":"$8,$9,"+"}' > ${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}_${CUTOFF}.bd_regions.bed
+cat $BD_OUT_FILE | awk '!/^#/ && $1 != $4 {OFS="\t"; print $1,$2,$2+1000,$7"("$1"-"$4"):"$8,$9,"+"}' >> ${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}_${CUTOFF}.bd_regions.bed
+cat $BD_OUT_FILE | awk '!/^#/ && $1 != $4 {OFS="\t"; print $4,$5,$5+1000,$7"("$1"-"$4"):"$8,$9,"+"}' >> ${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}_${CUTOFF}.bd_regions.bed
