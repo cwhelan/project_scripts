4ac6e69cfb59987f3d01def046467c14e9dc6c4a
Chris Whelan <cwhelan@gmail.com>
2012-08-16 14:17:34 -0700

    Add cutoff param to breakdancer

--- gene_rearrange/breakdancer_pe.sh
+++ gene_rearrange/breakdancer_pe.sh
@@ -13,15 +13,20 @@ if [[ -n "${3}" ]] ; then
     MIN_PAIRS=$3
 fi
 
+CUTOFF=4
+if [[ -n "${4}" ]] ; then
+    CUTOFF=$4
+fi
+
 WORKING_DIR=`dirname $BAM_FILE`
 FILE_NAME=`basename $BAM_FILE`
 FILE_PREFIX=`echo $FILE_NAME | awk -F. '{ print $1 }'`
 
-CONFIG_FILE=${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}.cfg
+CONFIG_FILE=${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}_${CUTOFF}.cfg
 
-bam2cfg.pl $WORKING_DIR/$BAM_FILE > $CONFIG_FILE
+bam2cfg.pl -c $CUTOFF -q $QUALITY $WORKING_DIR/$BAM_FILE > $CONFIG_FILE
 BD_OUT_FILE=${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}.bd_out.txt
-breakdancer_max -d ${FILE_PREFIX}.bdreads.bed -g ${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}.bd.bed $CONFIG_FILE -q $QUALITY -r $MIN_PAIRS > $BD_OUT_FILE
+breakdancer_max -d ${FILE_PREFIX}.bdreads.bed -g ${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}.bd.bed $CONFIG_FILE -q $QUALITY -r $MIN_PAIRS -c $CUTOFF > $BD_OUT_FILE
 
 #cat $BD_OUT_FILE | awk '!/^#/ {OFS="\t"; print $1,$2,$5,$7":"$8,$9,"+"}' > ${FILE_PREFIX}.bd_regions.bed
 cat $BD_OUT_FILE | awk '!/^#/ && $1 == $4 {OFS="\t"; print $1,$2,$5,$7":"$8,$9,"+"}' > ${FILE_PREFIX}_${QUALITY}_${MIN_PAIRS}.bd_regions.bed
