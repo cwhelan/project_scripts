b9f65ef3e050c5bd585fac9ab75d131874f448a4
Chris Whelan <cwhelan@gmail.com>
2012-11-07 15:07:26 -0800

    scripts to run delly

---
+++ gene_rearrange/delly.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -u
+
+BAM_FILE=$1
+REF=$2
+
+MAPQ=0
+if [[ -n "${3}" ]] ; then
+    MAPQ=$3
+fi
+
+MAD_CUTOFF=5
+if [[ -n "${4}" ]] ; then
+    MAD_CUTOFF=$4
+fi
+
+
+WORKING_DIR=`dirname $BAM_FILE`
+FILE_NAME=`basename $BAM_FILE`
+FILE_PREFIX=`echo $FILE_NAME | awk -F. '{ print $1 }'`
+
+PE_OUTFILE=${FILE_PREFIX}.delly_q${MAPQ}_c$MAD_CUTOFF}_del.txt
+SR_OUTFILE=${FILE_PREFIX}.delly_q${MAPQ}_c$MAD_CUTOFF}_br.txt
+
+delly -p -g $REF -q $MAPQ -c $MAD_CUTOFF $BAM_FILE -o $PE_OUTFILE -b $SR_OUTFILE
