d22e81bb952011596c26d1f1c8010c358aa214b4
Chris Whelan <cwhelan@gmail.com>
2012-11-21 10:10:06 -0800

    allow passing additional params to bwa

--- gene_rearrange/bwa_full.sh
+++ gene_rearrange/bwa_full.sh
@@ -10,14 +10,15 @@ OUTFILE_PREFIX=$4
 RGID=$5
 LIBRARY=$6
 SAMPLE=$7
+ADDITIONAL_BWA_ALN_PARAMS=$8
 
 READ1_BASE=`basename $READ_FILE1`
 READ2_BASE=`basename $READ_FILE2`
 
-echo "/g/whelanch/software/bin/bwa aln -t 8 $REFERENCE $READ_FILE1 > $READ1_BASE.sai"
-/g/whelanch/software/bin/bwa aln -t 8 $REFERENCE $READ_FILE1 > $READ1_BASE.sai
-echo "/g/whelanch/software/bin/bwa aln -t 8 $REFERENCE $READ_FILE2 > $READ2_BASE.sai"
-/g/whelanch/software/bin/bwa aln -t 8 $REFERENCE $READ_FILE2 > $READ2_BASE.sai
+echo "/g/whelanch/software/bin/bwa aln -t 8 $ADDITIONAL_BWA_ALN_PARAMS $REFERENCE $READ_FILE1 > $READ1_BASE.sai"
+/g/whelanch/software/bin/bwa aln -t 8 $ADDITIONAL_BWA_ALN_PARAMS $REFERENCE $READ_FILE1 > $READ1_BASE.sai
+echo "/g/whelanch/software/bin/bwa aln -t 8 $ADDITIONAL_BWA_ALN_PARAMS $REFERENCE $READ_FILE2 > $READ2_BASE.sai"
+/g/whelanch/software/bin/bwa aln -t 8 $ADDITIONAL_BWA_ALN_PARAMS $REFERENCE $READ_FILE2 > $READ2_BASE.sai
 
 
 echo "/g/whelanch/software/bin/bwa sampe -r ""'"@RG\tID:${RGID}\tLB:${LIBRARY}\tSM:${SAMPLE}"'"" $REFERENCE $READ1_BASE.sai $READ2_BASE.sai $READ_FILE1 $READ_FILE2 | samtools view -Sb - > $OUTFILE_PREFIX.bam"
