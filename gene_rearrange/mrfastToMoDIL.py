ffb22f1343a095adc3fa4a466a3cde2a6fd01646
Chris Whelan <cwhelan@gmail.com>
2012-11-24 12:06:52 -0800

    script to prep modil input

---
+++ gene_rearrange/mrfastToMoDIL.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("insert_size", type=int, help="Insert size of the library")
+args = parser.parse_args()
+
+library_isize = args.insert_size
+
+for line in sys.stdin:
+    if line.startswith('@'):
+        continue
+    fields = line.split("\t")
+    readid = fields[0]
+    flag = int(fields[1])
+    #only process first segments in template
+    pos = int(fields[4])
+    mpos = int(fields[7])
+    if (mpos < pos):
+        continue
+    tlen = int(fields[8])
+    sequence = fields[9]
+    slen = length(sequence)
+    endpos = pos + slen
+    
+    print "\t".join(map(str, [endpos, mpos, readid, library_isize - tlen, library_isize, readid]))
