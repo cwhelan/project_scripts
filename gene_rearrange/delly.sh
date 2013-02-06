8562426231530f2f6797687095833b23b52e1c85
Chris Whelan <cwhelan@gmail.com>
2013-02-06 14:49:58 -0800

    add timing information

--- gene_rearrange/delly.sh
+++ gene_rearrange/delly.sh
@@ -24,4 +24,9 @@ FILE_PREFIX=`echo $FILE_NAME | awk -F. '{ print $1 }'`
 PE_OUTFILE=${FILE_PREFIX}.delly_q${MAPQ}_c${MAD_CUTOFF}_del.txt
 SR_OUTFILE=${FILE_PREFIX}.delly_q${MAPQ}_c${MAD_CUTOFF}_br.txt
 
+echo "runing DELLY"
+date +%s
+echo "delly -p -g $REF -q $MAPQ -s $MAD_CUTOFF $BAM_FILE -o $PE_OUTFILE -b $SR_OUTFILE"
 delly -p -g $REF -q $MAPQ -s $MAD_CUTOFF $BAM_FILE -o $PE_OUTFILE -b $SR_OUTFILE
+echo "done"
+date +%s
